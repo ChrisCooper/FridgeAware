93fb0f418a69b01e1f28fccac22e4e2c9d0891ba
Azade <az.eftekhari@gmail.com>
2013-04-04 14:55:07 -0400

    Food name included in notification

--- src/com/cisc325/g3/fridgeaware/NotificationReceiver.java
+++ src/com/cisc325/g3/fridgeaware/NotificationReceiver.java
@@ -1,5 +1,8 @@
 package com.cisc325.g3.fridgeaware;
 
+import com.cisc325.g3.fridgeaware.models.FoodItem;
+import com.cisc325.g3.fridgeaware.sql.FoodItemDataSource;
+
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -11,19 +14,27 @@ public class NotificationReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
     	
-    	//long foodItemID = intent.getLongExtra("foodItemID", -1);
+    	long foodItemID = intent.getLongExtra("foodItemID", -1);
     	
+
+		FoodItemDataSource datasource = new FoodItemDataSource(context);
+        datasource.open();
+        
+        FoodItem foodItem = datasource.getFoodItem(foodItemID);
+       
         NotificationManager mNM;
         mNM = (NotificationManager)context.getSystemService(context.NOTIFICATION_SERVICE);
         // Set the icon, scrolling text and timestamp
        
         Notification notification = new Notification(R.drawable.ic_launcher, "Test Alarm",
         System.currentTimeMillis());
+        
         // The PendingIntent to launch our activity if the user selects this notification
         PendingIntent contentIntent = PendingIntent.getActivity(context, 0, new Intent(context, FoodItemListActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
+        
         // Set the info for the views that show in the notification panel.
-        notification.setLatestEventInfo(context, context.getText(R.string.app_name), "This is a Test Alarm", contentIntent);
-        //notification.setLatestEventInfo(context, context.getText(R.string.app_name), "This is a Test Alarm", PendingIntent.getActivity(context, 0, null, 0));
+        notification.setLatestEventInfo(context, foodItem.getName() + " will expire soon", context.getText(R.string.app_name), contentIntent);
+        
         // Send the notification.
         // We use a layout id because it is a unique number. We use it later to cancel.
         mNM.notify(R.string.app_name, notification);
